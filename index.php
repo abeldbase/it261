29a0c53babeceb1baf311ae955f5465f9d9bea29
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2024-01-05 15:55:08 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My portal page</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet" />
+  </head>
+  <body>
+
+<h1> <a href="index.php">Abel's Portal Page</a></h1>
+<h2>The navigation below represents our BIG Assigements.</h2>
+<div id="wrapper">
+<nav>
+          <ul>
+            <li><a href="">Switch</a></li>
+            <li><a href="">Troubleshoot</a></li>
+            <li><a href="">Calculator</a></li>
+            <li><a href="">Email</a></li>
+            <li><a href="">Database</a></li>
+            <li><a href="">Gallery</a></li>
+          </ul>
+        </nav>
+        <main>
+            <h2>About Abel </h2>
+            <img class="right" src="images/abel.jpg" alt="picture of Abel">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi quos esse, reprehenderit dignissimos dicta alias, quo fuga repellendus, at explicabo dolore reiciendis quisquam tenetur odit earum. Aperiam, ipsam? Dolores, rerum.</p>
+            
+        </main>
+    <aside>
+        <h2>Weekly class exercises</h2>
+        <h3>week 2</h3>
+        <ol>
+            <li><a href="var.php">var.php</li>
+            <li><a href="var2.php">var.php</li>
+            <li><a href="var3.php">var.php</li>
+            <li><a href="var4.php">var.php</li>
+        </ol>
+    </aside>
+</div>
+  </body>
+</html>
