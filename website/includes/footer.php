cecbee950fa9c9fc639105e256b5750848337884
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2024-02-28 14:30:07 -0800

    Add files via upload
    correction after comment from Olga , on week 8

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -1,9 +1,7 @@
-
 <footer>
 <ul>
     <li>&copy; 2024 by Abel Mengstu, All Rights Reserved ~</li>
-    <li><a id="html-checker" href="#" target="_blank">Check HTML</a> ~</li>
-    <li><a id="css-checker" href="#" target="_blank">Check CSS</a></li>
+    <li><a id="html-checker" href="#" target="_blank">Check HTML</a> ~ <a id="css-checker" href="#" target="_blank">Check CSS</a></li>
 </ul>
 <script>
     document.getElementById("html-checker").setAttribute("href", "https://validator.w3.org/nu/?doc=" + location.href);
