3e50fff8d5076bab548dc1adec036d2bbd0aa468
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2024-03-05 23:21:41 -0800

    Delete weeks/week9/credentials.php

--- weeks/week9/credentials.php
+++
@@ -1,5 +0,0 @@
-<?php   // my credentials that will allow me to connect to the database!!!
-define('DB_NAME','qslanjmy_winter2024');
-define('DB_USER','qslanjmy_abel');
-define('DB_PASSWORD','Hewan98133@');
-define('DB_HOST','localhost');
